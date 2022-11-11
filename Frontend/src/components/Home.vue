3f8ed2769b50cbe9b2b2e9ddfc3e2115165d45ae
Daniel <105089156+PleiniLeine@users.noreply.github.com>
2022-11-11 05:50:27 -0300

    upload do projeto

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="text-center">
     <div cols="12" class="d-flex justify-center mb-6 carrossel">
-        <v-carousel hide-delimiters cycle>
-          <v-carousel-item  reverse-transition="fade-transition" transition="fade-transition">
-            <img src="../assets/home/1.jpg" width="500">
-          </v-carousel-item>
-          <v-carousel-item  reverse-transition="fade-transition" transition="fade-transition">
-            <img src="../assets/home/2.jpg" width="500">
-          </v-carousel-item>
-          <v-carousel-item  reverse-transition="fade-transition" transition="fade-transition">
-            <img src="../assets/home/3.jpg" width="500">
-          </v-carousel-item>
-          <v-carousel-item  reverse-transition="fade-transition" transition="fade-transition">
-            <img src="../assets/home/4.jpg" width="500">
-          </v-carousel-item>
+      <v-carousel hide-delimiters cycle>
+        <v-carousel-item reverse-transition="fade-transition" transition="fade-transition">
+          <img src="../assets/home/1.jpg" width="500">
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="fade-transition" transition="fade-transition">
+          <img src="../assets/home/2.jpg" width="500">
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="fade-transition" transition="fade-transition">
+          <img src="../assets/home/3.jpg" width="500">
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="fade-transition" transition="fade-transition">
+          <img src="../assets/home/4.jpg" width="500">
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="fade-transition" transition="fade-transition">
+          <img src="../assets/home/5.jpg" width="500">
+        </v-carousel-item>
       </v-carousel>
     </div>
   </div>
-  
-    
+
+
 </template>
  
 <script>
